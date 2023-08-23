21021e8bd9253c6dcd0eb0be22bf37a29690e80e
vai-shali <vaishu.blr@gmail.com>
2023-08-23 23:57:39 +0530

    solution1

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,37 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    // Extract numbers from JSON payload
+    int num1 = input["first"].i();
+    int num2 = input["second"].i();
+
+    int result = num1 + num2;
+
+    // Create JSON response
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    // Return the response
+    return crow::response(response);
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    // Extract numbers from JSON payload
+    int num1 = input["first"].i();
+    int num2 = input["second"].i();
+
+    int result = num1 - num2;
+
+    // Create JSON response
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    // Return the response
+    return crow::response(response);
 }
